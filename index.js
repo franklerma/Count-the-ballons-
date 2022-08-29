79fd1f858213ebf56ff03de173de39c1492f3aaa
franklerma <109907966+franklerma@users.noreply.github.com>
2022-08-29 15:20:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let countEL = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
+let count = 0
+
+console.log(saveEL)
+
+function increment() {
+    count += 1
+    countEL.innerText = count
+}
+
+
+
+function save() {
+    let countStr = count + " - "
+    saveEL.textContent += countStr
+    countEL.textContent = 0
+    count = 0
+
+    console.log(count)
+}
+
